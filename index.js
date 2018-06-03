660b0dc428046a1d4ae25ba579ec1e0d1be9f810
Shanelle Savoy <mznellienellz@gmail.com>
2018-06-03 01:41:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var facts = [];
   var newArray = []
   var count = 0
   while(count < facts.length) {
