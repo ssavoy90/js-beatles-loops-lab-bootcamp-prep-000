5690f8c6f5d896d0c0e39574adfab79111d2af01
Shanelle Savoy <mznellienellz@gmail.com>
2018-06-03 02:16:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var emptyArray = [];
   for(var i = 0; i < musicians.length; i++) {
-    emptyArray.push(musicians + "plays" + instruments);
+    emptyArray.push(musicians[i] + "plays" + instruments[i]);
   }
   return emptyArray
 }
